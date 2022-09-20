af3e34e349bbddb5071418597ad20dcf21fbf45a
Adrien Lloret <60384986+lloret-adrien@users.noreply.github.com>
2022-09-20 19:31:52 +0200

    Update conf.php

--- config/conf.php
+++ config/conf.php
@@ -2,18 +2,10 @@
 class Conf {
    
   static private $databases = array(
-    // Le nom d'hote est webinfo a l'IUT
-    // ou localhost sur votre machine
-    'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
-    // A l'IUT, vous avez une BDD nommee comme votre login
-    // Sur votre machine, vous devrez creer une BDD
-    'database' => 'lloreta',
-    // A l'IUT, c'est votre login
-    // Sur votre machine, vous avez surement un compte 'root'
-    'login' => 'lloreta',
-    // A l'IUT, c'est votre mdp (INE par defaut)
-    // Sur votre machine personelle, vous avez creez ce mdp a l'installation
-    'password' => '071256198AC'
+    'hostname' => '',
+    'database' => '',
+    'login' => '',
+    'password' => ''
   );
 
   static private $debug = True;
